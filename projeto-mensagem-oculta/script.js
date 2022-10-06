48d2d711d2b23d63239c08c5583650cf355de466
Amanda Freitas <amandaafreitas20@gmail.com>
2022-10-06 09:54:54 -0300

    Cifra de cesar funcionando

--- projeto-mensagem-oculta/script.js
+++ projeto-mensagem-oculta/script.js
@@ -1,17 +1,70 @@
-function cifraDeCesar(frase) {
-  let nomeCodificado = "";
-  let posicoes = 2;
+// Botao de cesar ativo
 
-  for (let i = 0; i < frase.length; i++) {
-    let idLetra = frase.charCodeAt(i);
+let btnCesar = document.querySelector("#cesar");
+let btnBase64 = document.querySelector("#base");
+let incremento = document.querySelector(".incre");
+let radioCodificar = document.querySelector("#radioCodificar");
+let radioDecodificar = document.querySelector("#radioDecodificar");
+let btnCodificar = document.querySelector("#btncodificar");
+let btnDecodificar = document.querySelector("#btndecodificar");
+
+btnBase64.addEventListener("click", desativaIncremento);
+btnCesar.addEventListener("click", ativaIncremento);
+radioCodificar.addEventListener("click", ativarBtnCodificar);
+radioDecodificar.addEventListener("click", ativarBtnDecodificar);
+btnCodificar.addEventListener("click", codificarCifraDeCesar);
+
+function ativaIncremento() {
+  incremento.style.display = "block";
+}
+
+function desativaIncremento() {
+  incremento.style.display = "none";
+}
+
+function ativarBtnCodificar() {
+  btnCodificar.style.display = "inline-block";
+  btnDecodificar.style.display = "none";
+}
+
+function ativarBtnDecodificar() {
+  btnDecodificar.style.display = "inline-block";
+  btnCodificar.style.display = "none";
+}
+
+function codificarCifraDeCesar() {
+  let msgUsuario = document.querySelector("#msg-user").value;
+  let inputMsgCodificada = document.querySelector("#msg-criptografada");
+  let incrementoInput = Number(document.querySelector("#incremento").value);
+
+  let fraseCodificada = "";
+
+  for (let i = 0; i < msgUsuario.length; i++) {
+    let idLetra = msgUsuario.charCodeAt(i);
     if ((idLetra >= 65 && idLetra <= 90) || (idLetra >= 97 && idLetra <= 122)) {
-      nomeCodificado += String.fromCharCode(idLetra + posicoes);
+      fraseCodificada += String.fromCharCode(idLetra + incrementoInput);
     } else {
-      nomeCodificado += frase[i];
+      fraseCodificada += msgUsuario[i];
     }
   }
 
-  return nomeCodificado;
+  inputMsgCodificada.innerHTML = fraseCodificada;
 }
 
-console.log(cifraDeCesar("Oi, eu sou um dev!"));
+// function cifraDeCesar(frase) {
+//   let fraseCodificada = "";
+//   let posicoes = 2;
+
+//   for (let i = 0; i < frase.length; i++) {
+//     let idLetra = frase.charCodeAt(i);
+//     if ((idLetra >= 65 && idLetra <= 90) || (idLetra >= 97 && idLetra <= 122)) {
+//       fraseCodificada += String.fromCharCode(idLetra + posicoes);
+//     } else {
+//       fraseCodificada += frase[i];
+//     }
+//   }
+
+//   return fraseCodificada;
+// }
+
+// console.log(cifraDeCesar("Oi, eu sou um dev!"));
